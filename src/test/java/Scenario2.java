f17b02f0d0dfcfe8136cd96ac9ab5982b93fdf3a
Krishna Bhagavan <krishna_bandi1@epam.com>
2022-01-28 15:27:10 +0530

    New Changes

---
+++ src/test/java/Scenario2.java
@@ -0,0 +1,7 @@
+public class Scenario2 {
+
+    public static void main(String[] args){
+
+        System.out.println("This is from Scenario2");
+    }
+}
---
+++ src/test/java/Scenario3.java
@@ -0,0 +1,7 @@
+public class Scenario3 {
+
+    public static void main(String[] args){
+
+        System.out.println("This is from Scenario1");
+    }
+}
