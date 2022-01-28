2fc4b5d0f71bd08503fcf103f19a51fb026bc21b
Krishna Bhagavan <krishna_bandi1@epam.com>
2022-01-28 15:33:42 +0530

    Create_Delete_Modify

--- src/test/java/Sceanrio1.java
+++ src/test/java/Sceanrio1.java
@@ -3,5 +3,6 @@ public class Sceanrio1 {
     public static void main(String[] args){
 
         System.out.println("This is from Scenario1");
+        System.out.println("Welcome to Scenario1");
     }
 }
