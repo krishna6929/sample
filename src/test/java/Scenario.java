75c7b517c5512e80b2b15c6e8b6f18e7a8554fa2
Krishna Bhagavan <krishna_bandi1@epam.com>
2022-01-28 15:48:29 +0530

    Feature

---
+++ src/test/java/Scenario.java
@@ -0,0 +1,8 @@
+public class Scenario {
+
+    public static void main(String[] args){
+
+        System.out.println("This is from Scenario");
+        System.out.println("Welcome to Scenario");
+    }
+}
